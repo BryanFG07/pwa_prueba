260706bfb9adfc11d1e144f818e6490decc4e55d
BryanFG07 <bryan.flores3214@gmail.com>
2024-06-06 10:50:11 -0500

    Cambio de mensaje

--- src/public/js/formRiesgo.js
+++ src/public/js/formRiesgo.js
@@ -28,7 +28,7 @@ function enviarDatosAlServidor(data) {
         }
     }).then(response => {
         if (response.ok) {
-            alert("Registro guardado correctamente, folio: " + data.id);
+            alert("Registro guardado correctamente" );
             deleteForm(data.id);
         } else {
             return Promise.reject('Failed to send data to server');
